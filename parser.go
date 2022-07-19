86cd17757b80a6eae6cbff07ffb6136a583d7bc5
Alia Saddik <82057274+aliasaddik@users.noreply.github.com>
2022-07-19 18:43:20 +0200

    test and errors

--- parser.go
+++ parser.go
@@ -19,45 +19,73 @@ func isSection(line string) bool {
 func isKey(line string) bool {
 	return strings.Contains(line, "=")
 }
+func isComment(line string) bool {
+	return strings.HasPrefix(line, ";")
+
+}
 
 // alia=\"all=aaa\"
-func (p *parsed) Parse() {
-
+func (p *parsed) parse() error {
+	p.ParsedValues = make(map[string]map[string]string)
 	scanner := bufio.NewScanner(strings.NewReader(p.file))
 	var section string
 	for scanner.Scan() {
 		myLine := strings.Trim(scanner.Text(), " ")
-
+		// []
 		if isSection(myLine) {
+
 			section = myLine[1 : len([]rune(myLine))-1]
+			section = strings.Trim(section, " ")
+			if section == "" {
+				return errors.New("the Section name is empty")
+
+			}
+
 			p.ParsedValues[section] = make(map[string]string)
 			continue
 		}
 
 		if isKey(myLine) {
+			if strings.HasPrefix(myLine, "=") {
+				return errors.New("invalid '=' sign")
+			}
 			s := strings.Split(myLine, "=")
+
+			if len(s) > 2 {
+				return errors.New("invalid '=' sign")
+			}
 
 			p.ParsedValues[section][strings.Trim(s[0], " ")] = strings.Trim(s[1], " ")
 
 			continue
 		}
+		if isComment(myLine) || myLine == "" {
+			continue
+		} else {
+			return errors.New("invalid ini line")
+		}
 	}
+	return nil
 }
 
 func (p *parsed) LoadFromFile(filepath string) error {
+	if filepath[len([]rune(filepath))-3:len([]rune(filepath))] != "ini" {
+		return errors.New("not an ini file")
+
+	}
 	data, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return err
 	}
+
 	p.file = string(data)
-	p.Parse()
-	return nil
+	return p.parse()
+
 }
 
 func (p *parsed) LoadFromString(inputFile string) error {
 	p.file = inputFile
-	p.Parse()
-	return nil
+	return p.parse()
 }
 
 func (p *parsed) GetSectionNames() ([]string, error) {
@@ -69,19 +97,31 @@ func (p *parsed) GetSectionNames() ([]string, error) {
 		res = append(res, sect)
 
 	}
+	if len(res) == 0 {
+		return res, errors.New("there is no sections in the file")
+	}
 
 	return res, nil
 
 }
 func (p *parsed) GetSections() (map[string]map[string]string, error) {
+
 	if p.file == "" {
 		return nil, errors.New("there is no file loaded")
+	}
+
+	if len(p.ParsedValues) == 0 {
+		return nil, errors.New("there is no sections in the file")
 	}
 
 	return p.ParsedValues, nil
 }
 
 func (p *parsed) Get(section, key string) (string, error) {
+
+	if p.file == "" {
+		return "", errors.New("there is no file loaded")
+	}
 
 	value, ok := p.ParsedValues[section][key]
 	if !ok {
@@ -93,24 +133,40 @@ func (p *parsed) Get(section, key string) (string, error) {
 }
 
 func (p *parsed) Set(section, key, newValue string) error {
+	if strings.TrimSpace(section) == "" || strings.TrimSpace(key) == "" {
+		return errors.New("input cannot be empty")
+	}
+	_, ok := p.ParsedValues[section]
+	if !ok {
+		p.ParsedValues[section] = make(map[string]string)
 
+	}
 	p.ParsedValues[section][key] = newValue
+	p.toString()
 	return nil
 }
 
-func (p *parsed) ToString() {
+func (p *parsed) toString() {
 	file := ""
 	for sect, keysAndVals := range p.ParsedValues {
-		file += "[" + sect + "] "
+		file += "[" + sect + "] \n"
 		for key, value := range keysAndVals {
-			file += key + " = " + value + " "
+			file += key + " = " + value + "\n "
 		}
 	}
 	p.file = file
 
 }
 
-func (p *parsed) SaveToFile(filepath string) {
+func (p *parsed) SaveToFile(filepath string) error {
+	if strings.TrimSpace(filepath) == "" {
+		return errors.New(" file path is empty")
+	}
+	if filepath[len([]rune(filepath))-3:len([]rune(filepath))] != "ini" {
+		return errors.New("wrong file externsion provided")
+	}
+
 	dataBytes := []byte(p.file)
 	ioutil.WriteFile(filepath, dataBytes, 0)
+	return nil
 }
--- parser_test.go
+++ parser_test.go
@@ -5,46 +5,268 @@ import (
 	"testing"
 )
 
-func TestGetSects(t *testing.T) {
+func TestLoadFrom(t *testing.T) {
+
 	parser := parsed{}
 	parser.ParsedValues = map[string]map[string]string{}
 	parser.file = ""
-	parser.LoadFromFile("testing.ini")
+	t.Run("File not found", func(t *testing.T) {
+		got := parser.LoadFromFile("blaa.ini")
+		if got == nil {
+			t.Errorf("Error file not found not handled")
+		}
+	})
 
-	got, _ := parser.GetSectionNames()
+	t.Run("Wrong extension of file", func(t *testing.T) {
+		got := parser.LoadFromFile("testing.txt")
+		if got == nil {
+			t.Errorf("Error wrong extension not handled")
+		}
+	})
 
-	want := []string{"owner", "database"}
+	t.Run("Section is empty", func(t *testing.T) {
+		got := parser.LoadFromString(
+			`; last modified 1 April 2001 by John Doe 
+			   [  ] 
+			   name = JohnDoe 
+			   organization = AcmeWidgetsInc.
+			   [database]
+			   ; use IP address in case network name resolution is not working
+			   server = 192.0.2.62     
+			   port = 143
+			   file = \"payroll.dat\`)
+		if got == nil {
+			t.Errorf("section is empty is not handled")
+		}
+	})
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	t.Run("wrong equal sign", func(t *testing.T) {
+		got := parser.LoadFromString(
+			`; last modified 1 April 2001 by John Doe 
+				   [owner  ] 
+				    = johndoe
+				   organization = AcmeWidgetsInc.
+				   [database]
+				   ; use IP address in case network name resolution is not working
+				   server = 192.0.2.62     
+				   port = 143
+				   file = \"payroll.dat\`)
+		if got == nil {
+			t.Errorf("wrong equal sign is not handled")
+		}
+
+	})
+	t.Run("invalid line", func(t *testing.T) {
+		got := parser.LoadFromString(
+			`blaaaa`)
+		if got == nil {
+			t.Errorf("invalid line not handled")
+		}
+	})
+	t.Run("invalid line", func(t *testing.T) {
+		got := parser.LoadFromString(
+			`[blaaaa
+			  binary = true`)
+		if got == nil {
+			t.Errorf("invalid line not handled")
+		}
+	})
+
+}
+func TestGetSectionNames(t *testing.T) {
+
+	parser := parsed{}
+	parser.ParsedValues = map[string]map[string]string{}
+	parser.file = ""
+
+	t.Run("Empty file", func(t *testing.T) {
+		parser.LoadFromString(" ")
+		_, err := parser.GetSectionNames()
+
+		if err == nil {
+			t.Errorf("empty file not handled")
+		}
+	})
+	t.Run("invalid line", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+
+		got, _ := parser.GetSectionNames()
+		want := []string{"owner", "database"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
 }
 
 func TestGetSections(t *testing.T) {
-	p := parsed{}
-	p.ParsedValues = map[string]map[string]string{}
-	p.file = ""
-	p.LoadFromFile("testing.ini")
+	parser := parsed{}
+	parser.ParsedValues = map[string]map[string]string{}
+	parser.file = ""
 
-	got, _ := p.GetSections()
+	t.Run("Empty file", func(t *testing.T) {
+		parser.LoadFromString(" ")
+		_, err := parser.GetSections()
 
-	want := map[string]map[string]string{"owner": {"name": "JohnDoe", "organization": "AcmeWidgetsInc."}, "database": {"server": "192.0.2.62", "port": "143", "file": "\"payroll.dat\""}}
+		if err == nil {
+			t.Errorf("empty file not handled")
+		}
+	})
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	t.Run("Normal Input", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+		got, _ := parser.GetSections()
+
+		want := map[string]map[string]string{"owner": {"name": "JohnDoe", "organization": "AcmeWidgetsInc."}, "database": {"server": "192.0.2.62", "port": "143", "file": "\"payroll.dat\""}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
 }
 func TestGet(t *testing.T) {
-	p := parsed{}
-	p.ParsedValues = map[string]map[string]string{}
-	p.file = ""
-	p.LoadFromFile("testing.ini")
+	parser := parsed{}
+	parser.ParsedValues = map[string]map[string]string{}
+	parser.file = ""
 
-	got, _ := p.Get("database", "port")
+	t.Run("Empty file", func(t *testing.T) {
+		parser.LoadFromString(" ")
+		_, err := parser.Get("database", "port")
+		if err == nil {
+			t.Errorf("empty file not handled")
+		}
+	})
+	t.Run("Normal file", func(t *testing.T) {
 
-	want := "143"
+		parser.LoadFromFile("testing.ini")
+		got, _ := parser.Get("database", "port")
+		want := "143"
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("Key not found", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
+		parser.LoadFromFile("testing.ini")
+		_, err := parser.Get("database", "dalas")
+
+		if err == nil {
+
+			t.Errorf("error key not found is not handled")
+		}
+	})
+	t.Run("section not found", func(t *testing.T) {
+
+		parser.LoadFromFile("testing.ini")
+		_, err := parser.Get("databse", "port")
+
+		if err == nil {
+			t.Errorf("error section not found is not handled")
+		}
+	})
+
+}
+
+func TestSet(t *testing.T) {
+	parser := parsed{}
+	parser.ParsedValues = map[string]map[string]string{}
+	parser.file = ""
+
+	t.Run("Empty section", func(t *testing.T) {
+
+		got := parser.Set("  ", "first", "myfirstname")
+		if got == nil {
+			t.Errorf("error section is empty is not handled")
+		}
+	})
+	t.Run("Empty key", func(t *testing.T) {
+
+		got := parser.Set("name  ", "  ", "myfirstname")
+		if got == nil {
+			t.Errorf("error key is empty is not handled")
+		}
+	})
+
+	t.Run("Empty file", func(t *testing.T) {
+		parser.Set("name", "first", "myfirstname")
+		got, _ := parser.GetSections()
+		want := map[string]map[string]string{"name": {"first": "myfirstname"}}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
+	t.Run("non present section", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+		parser.Set("name", "first", "myfirstname")
+		got, _ := parser.GetSections()
+
+		want := map[string]map[string]string{"owner": {"name": "JohnDoe", "organization": "AcmeWidgetsInc."}, "database": {"server": "192.0.2.62", "port": "143", "file": "\"payroll.dat\""}, "name": {"first": "myfirstname"}}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
+	t.Run("non present key", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+		parser.Set("owner", "age", "22")
+		got, _ := parser.GetSections()
+
+		want := map[string]map[string]string{"owner": {"name": "JohnDoe", "organization": "AcmeWidgetsInc.", "age": "22"}, "database": {"server": "192.0.2.62", "port": "143", "file": "\"payroll.dat\""}}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("change value", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+		parser.Set("owner", "name", "aliasaddik")
+		got, _ := parser.GetSections()
+
+		want := map[string]map[string]string{"owner": {"name": "aliasaddik", "organization": "AcmeWidgetsInc."}, "database": {"server": "192.0.2.62", "port": "143", "file": "\"payroll.dat\""}}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
+
+func TestSaveToFile(t *testing.T) {
+	parser := parsed{}
+	parser.ParsedValues = map[string]map[string]string{}
+	parser.file = ""
+
+	t.Run("wrong extention error", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+		got := parser.SaveToFile("new.txt")
+		if got == nil {
+			t.Errorf("wrong extension not handled")
+		}
+
+	})
+	t.Run("empty path", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+		got := parser.SaveToFile(" ")
+		if got == nil {
+			t.Errorf("empty path error not handled")
+		}
+
+	})
+	t.Run("normal file", func(t *testing.T) {
+		parser.LoadFromFile("testing.ini")
+
+		parser.SaveToFile("new.ini")
+
+		parser.LoadFromFile("new.ini")
+
+		got, _ := parser.GetSections()
+		want := map[string]map[string]string{"owner": {"name": "JohnDoe", "organization": "AcmeWidgetsInc."}, "database": {"server": "192.0.2.62", "port": "143", "file": "\"payroll.dat\""}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
+	})
 }
